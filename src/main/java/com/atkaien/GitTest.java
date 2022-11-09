1d74784da465720b32cad92ce477e10b8d580a24
李开恩 <349264976@qq.com>
2022-11-09 20:14:43 +0800

    hotfixcommit

--- src/main/java/com/atkaien/GitTest.java
+++ src/main/java/com/atkaien/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git真不错！！！");
         System.out.println("git 真不错22222");
+        System.out.println("git 真不错hotfix");
     }
 }
